bae016f56e393dd855adb981b13666fef7fa21a2
Oliver Ziegler <oliver.ziegler@webfactor.de>
2018-02-08 18:15:42 +0100

    add config and update dependencies

---
+++ database/migrations/create_opening_hours_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOpeningHoursTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('opening_hours', function(\Illuminate\Database\Schema\Blueprint $table) {
+            $table->increments('id');
+            $table->morphs('openable');
+            $table->enum('day', [
+                'monday',
+                'tuesday',
+                'wednesday',
+                'thursday',
+                'friday',
+                'saturday',
+                'sunday'
+            ]);
+            $table->time('start');
+            $table->time('end');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('opening_hours');
+    }
+}
